1adca91867e6cee0936584c9455e9f65d9d2d5dd
rivansp <rvnsyahp@gmail.com>
2021-12-07 15:53:53 +0700

    Wedding Done

--- application/modules/Home/controllers/Home.php
+++ application/modules/Home/controllers/Home.php
@@ -97,8 +97,8 @@ class Home extends MY_Controller
 		$data['Image']				= $data['imagesPath'] . 'cover_mobile.webp';
 		$data['Song']				= $song;
 
-		var_dump($data);
-		// return view('Home/views/index', $data);
+		// var_dump($data);
+		return view('Home/views/index', $data);
 	}
 
 	public function test()
