83f270b15fa6b0371e9beda7645492e9123a825a
rivansp <rvnsyahp@gmail.com>
2021-12-07 15:42:33 +0700

    Wedding Done

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,6 +1,10 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+
 class Api extends MY_Controller
 {
 
