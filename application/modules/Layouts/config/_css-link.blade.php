d60446a81f5857c05fecb6d802592f02774ed2a0
rivansp <rvnsyahp@gmail.com>
2021-12-08 23:49:05 +0700

    Wedding tes

--- application/modules/Layouts/config/_css-link.blade.php
+++ application/modules/Layouts/config/_css-link.blade.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="{{ base_url('vendor/twbs/bootstrap/dist/css/bootstrap.min.css') }}">
 <link rel="stylesheet" href="{{ base_url('vendor/twbs/bootstrap-icons/bootstrap-icons.css') }}">
 
-<link href="{{ base_url('assets/styles/animation.css') }}" rel="stylesheet">
-<link href="{{ base_url('assets/styles/source.css') }}" rel="stylesheet">
-<link href="{{ base_url('assets/styles/source-tablet.css') }}" rel="stylesheet">
-<link href="{{ base_url('assets/styles/source-desktop.css') }}" rel="stylesheet">
+{{-- <link href="{{ base_url('assets/styles/animation.css') }}" rel="stylesheet"> --}}
+<link href="{{ base_url('assets/styles/source.min.css') }}" rel="stylesheet">
+<link href="{{ base_url('assets/styles/source-tablet.min.css') }}" rel="stylesheet">
+<link href="{{ base_url('assets/styles/source-desktop.min.css') }}" rel="stylesheet">
